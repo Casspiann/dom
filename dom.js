4a953b26a8047a5fcd936e95c21919117730fe13
Casspiann <tarunrana4221>
2023-08-03 00:45:03 +0530

    add second file

---
+++ dom.js
@@ -0,0 +1,14 @@
+//console.log(document.getElementById("header-title"));
+var headertitle = document.querySelector(".title");
+console.log(headertitle);
+var header = document.getElementById('main-header');
+//headertitle.textContent="hello";
+header.style.border="solid 3px #000";
+headertitle.style.fontWeight = "bold";
+headertitle.style.color = "green";
+//header.innerHTML="Good By";
+/*document.addEventListener("DOMContentLoaded", function() {
+    const addItemsTitle = document.querySelector(".title");
+    addItemsTitle.style.fontWeight = "bold";
+    addItemsTitle.style.color = "green";
+  });*/
