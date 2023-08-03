6236b6cfcce9a676ebbf7fc2c4515b4e8fae2b05
Casspiann <tarunrana4221>
2023-08-03 22:48:25 +0530

    add file

--- dom.js
+++ dom.js
@@ -28,9 +28,71 @@ console.log(li[1]);
 li[1].textContent="Hello";
 li[1].style.fontWeight="bold";
 li[4].className = "list-group-item";
-//li[1].style.backgroundColor="green";*/
+//li[1].style.backgroundColor="green";
 var li = document.querySelectorAll("li");
 li[1].style.backgroundColor="green";
 li[2].style.display="none";
-li[4].className = "list-group-item";
+li[4].className = "list-group-item";*/
+//parentNode
+var itemList = document.querySelector('#items');
+//console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "#f4f4f4";
+
+//parentElement
+//console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor="#f4f4f4";
+//console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNode
+//console.log(itemList.childNodes);
+//console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = "yellow";
+
+//FirstChild
+//console.log(itemList.firstChild);
+//console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = "hello";
+
+//lastChild
+//console.log(itemList.lastChild);
+//console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent="hello4";
+
+//nextSibling
+//console.log(itemList.previousSibling);
+
+//next Element Sibling
+//console.log(itemList.nextElementSibling);
+
+//previous Sibling
+//console.log(itemList.previousSibling);
+//console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color = "green"; 
+
+var newDiv = document.createElement("div");
+newDiv.className = "hello";
+newDiv.id = "hello1";
+newDiv.setAttribute("title", "HELLo");
+
+var newDIvText = document.createTextNode("HELLo");
+newDiv.appendChild(newDIvText);
+//console.log(newDiv);
+
+var container = document.querySelector('header .container');
+//var li = document.querySelector("li");
+var h1 = document.querySelector("header h1");
+container.insertBefore(newDiv,h1);
+//li.insertBefore(  )
+
+var itemListt = document.querySelector("#items");
+var listItem1 = itemList.querySelector("li");
+itemListt.insertBefore(newDiv, listItem1);
+
+
+
+
+
+
+
+
 
